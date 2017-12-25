d7c3d21956fd09f571842087794cc68e9c55dcb2
Leticia Sayuri <leticiasyrk@gmail.com>
2017-12-24 22:33:47 -0200

    [Aula 01] Substituindo implementação da classe TestandoListas para realização dos exercícios propostos.

--- gerenciador-de-cursos/src/br/com/alura/TestandoListas.java
+++ gerenciador-de-cursos/src/br/com/alura/TestandoListas.java
@@ -1,48 +1,19 @@
 package br.com.alura;
 
 import java.util.ArrayList;
-import java.util.Collections;
 
 public class TestandoListas {
 
 	public static void main(String[] args) {
-		String aula1 = "Conhecendo mais de listas";
-		String aula2 = "Modelando a classe Aula";
-		String aula3 = "Trabalhando com Cursos e Sets";
-
-		ArrayList<String> aulas = new ArrayList<>();
-		aulas.add(aula1);
-		aulas.add(aula2);
-		aulas.add(aula3);
-
-		System.out.println(aulas);
-
-		aulas.remove(0);
-		System.out.println(aulas);
-
-		for (String aula : aulas) {
-			System.out.println("Aula: " + aula);
-		}
-
-		String primeiraAula = aulas.get(0);
-		System.out.println("A primeira aula é " + primeiraAula);
-
-		for (int i = 0; i < aulas.size(); i++) {
-			System.out.println("Aula: " + i + " > " + aulas.get(i));
-		}
-
-		System.out.println(aulas.size());
-
-		//Para cada aula dentro de aulas, execute o que está dentro de {}
-		aulas.forEach(aula -> {
-			System.out.println("Percorrendo. . .");
-			System.out.println("Aula: " + aula);
-		});
+		String curso1 = "Design Patterns Java I: Boas práticas de programação";
+		String curso2 = "Testes: TDD com Java";
+		String curso3 = "Java: Dominando as collections";
 		
-		aulas.add("Aumentando nosso conhecimento");
-		System.out.println(aulas);
+		ArrayList<String> cursos = new ArrayList<>();
+		cursos.add(curso1);
+		cursos.add(curso2);
+		cursos.add(curso3);
 		
-		Collections.sort(aulas);
-		System.out.println(aulas);
+		System.out.println(cursos);
 	}
 }
