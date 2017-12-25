073a4448fd62473a8bc2adbdc5940c132b49d542
Leticia Sayuri <leticiasyrk@gmail.com>
2017-12-25 21:52:01 -0200

    [Aula 04] Criação de método que retorna o tempo total do curso (getTempoTotal()).

--- gerenciador-de-cursos/test/br/com/alura/TestaCurso.java
+++ gerenciador-de-cursos/test/br/com/alura/TestaCurso.java
@@ -1,5 +1,9 @@
 package br.com.alura;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class TestaCurso {
 
 	public static void main(String[] args) {
@@ -7,9 +11,16 @@ public class TestaCurso {
 
 		javaColecoes.adiciona(new Aula("Trabalhando com ArrayList", 21));
 		javaColecoes.adiciona(new Aula("Criando uma Aula", 20));
-		javaColecoes.adiciona(new Aula("Modelando com Coleções", 22));
+		javaColecoes.adiciona(new Aula("Modelando com Coleções", 24));
 		
-		System.out.println(javaColecoes.getAulas());
+		List<Aula> aulasImutaveis = javaColecoes.getAulas();
+		System.out.println(aulasImutaveis);
+		
+		List<Aula> aulas = new ArrayList<>(aulasImutaveis);
+		
+		Collections.sort(aulas);
+		System.out.println(aulas);
+		System.out.println(javaColecoes.getTempoTotal());
 	}
 
 }
