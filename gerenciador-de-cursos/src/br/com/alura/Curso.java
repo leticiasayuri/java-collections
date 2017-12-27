54a845d693fbf567d92422970589f9cb420141bc
Leticia Sayuri <leticiasyrk@gmail.com>
2017-12-27 14:33:47 -0200

    [Aula 10] Utilizando Mapas.

--- gerenciador-de-cursos/src/br/com/alura/Curso.java
+++ gerenciador-de-cursos/src/br/com/alura/Curso.java
@@ -2,8 +2,11 @@ package br.com.alura;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.Set;
 
 public class Curso {
@@ -15,6 +18,8 @@ public class Curso {
 	private List<Aula> aulas = new ArrayList<>();
 	
 	private Set<Aluno> alunos = new HashSet<>();
+	
+	private Map<Integer, Aluno> matriculaParaAluno = new HashMap<>();
 
 	public Curso(String nome, String instrutor) {
 		this.nome = nome;
@@ -53,6 +58,14 @@ public class Curso {
 		this.alunos = alunos;
 	}
 
+	public Map<Integer, Aluno> getMatriculaParaAluno() {
+		return matriculaParaAluno;
+	}
+
+	public void setMatriculaParaAluno(Map<Integer, Aluno> matriculaParaAluno) {
+		this.matriculaParaAluno = matriculaParaAluno;
+	}
+
 	public void adiciona(Aula aula) {
 		this.aulas.add(aula);
 	}
@@ -63,10 +76,18 @@ public class Curso {
 	
 	public void matricula(Aluno aluno) {
 		this.alunos.add(aluno);
+		this.matriculaParaAluno.put(aluno.getNumeroMatricula(), aluno);
 	}
 	
 	public boolean estaMatriculado(Aluno aluno) {
 		return this.alunos.contains(aluno);
+	}	
+	
+	public Aluno buscaAlunoPorMatricula(int matricula) {
+		if (!matriculaParaAluno.containsKey(matricula)) {
+			throw new NoSuchElementException();
+		}
+		return matriculaParaAluno.get(matricula);
 	}	
 	
 	@Override
